d68342a19a976aee0c87da7bfbed08ba684d9b84
HAKKI AYMAN <82755715+SoulCriminal@users.noreply.github.com>
2021-11-07 17:57:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz!");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int sayac = 1 ;
+            int toplam=0;
+        
+            while(sayac<=sayi){
+                toplam += sayac;
+                sayac ++;
+
+            }
+            Console.WriteLine("Ortalaması" +toplam/sayi);
+
+            // a dan z ye kadar alfabeyi yazdıralım
+
+            char character = 'a';
+            while(character<'z'){
+                Console.Write(character);
+                character ++;
+            }
+
+            // FOREACH
+
+            string [] cars = {"bmw", "porche","mercedes"};
+
+            foreach(var item in cars){
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
